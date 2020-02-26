d53bb63ae418170806f94b4339fea4c8ee347292
fayssal404 <fayssal.ds.stat@gmail.com>
2020-02-26 23:07:29 +0100

    Delete

--- EULER/problem1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3.6m
-
-import numpy as np
-#multiples of 3 or 5 under 10 
-def multiples(a,b,maxim):
-    multiples =[i for i in range(1,maxim) if (i %a ==0) or (i%5 ==0)]
-    return multiples, np.sum(multiples)
-
-#sum element of a list
